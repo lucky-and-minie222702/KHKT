fde4892172927eeab95489ce684f2dfcc99557ee
techn0z <minhtrietamoled@gmail.com>
2025-10-16 09:16:06 +0700

    update2

--- base.py
+++ base.py
@@ -77,6 +77,8 @@ class ModelInterface:
             pbar = tqdm(dl)
             for batch in pbar:
                 batch = {k: v.to(self.model.device) for k, v in batch.items()}
+                print(batch)
+                exit()
                 losses.append(self.model(**batch).loss.item())
                 pbar.set_postfix(loss = np.mean(losses))
         
